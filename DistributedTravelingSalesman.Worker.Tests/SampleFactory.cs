75c1e6eecacbf0f695d972b677c7de8d905a7e7a
toyerovsky <toyerek@gmail.com>
2021-11-20 17:07:08 +0100

    Add new project structure, city class with tests

---
+++ DistributedTravelingSalesman.Worker.Tests/SampleFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+using DistributedTravelingSalesman.Worker.Domain;
+
+namespace DistributedTravelingSalesman.Worker.Tests
+{
+    public static class SampleFactory
+    {
+        private static int _charSequence = 'A';
+        private static Random _random = new Random();
+        
+        public static City GetCity()
+        {
+            return new City
+            {
+                Name = $"City {_charSequence++}",
+                Location = new Vector2(_random.NextSingle(), _random.NextSingle())
+            };
+        }
+    }
+}
